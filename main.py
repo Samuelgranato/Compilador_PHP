6d19876eb480ba6965ba12e76490b85c80c457b7
samuelgranatoshopper <samuelgranato87@gmail.com>
2020-02-29 16:47:23 -0300

    tokenizer done

--- main.py
+++ main.py
@@ -1,31 +1,96 @@
 import sys
 
-source = sys.argv[1]
-i = 0
-while i < len(source) - 1:
-    if source[i].isdigit():
-        if source[i + 1] == " ":
-            i += 2
-            while i < len(source) - 1 and source[i] == " ":
-                i += 1
-            if i < len(source) - 1 and source[i].isdigit():
+class Token:
+    def __init__(self,token_type):
+        self.type = token_type
+        self.value = ''
+
+class Tokenizer:
+    def __init__(self,origin):
+        self.origin = origin
+        self.position = 0
+        self.selectNext()
+
+    def get_type(character):
+        if character == '+':
+            return 'plus'
+        if character == '-':
+            return 'minus'
+        if character == ' ':
+            return 'space'
+        if character.isdigit():
+            return 'int'
+
+    def selectNext(self):
+        if self.position == len(self.origin):
+            next_token = Token('EOF')
+            self.actual = next_token
+            return
+
+        current_char = self.origin[self.position]
+        while(current_char == ' '):
+            self.position += 1
+            if self.position == len(self.origin):
+                next_token = Token('EOF')
+                self.actual = next_token
+                return
+
+            current_char = self.origin[self.position]
+            
+        next_token = Token(Tokenizer.get_type(current_char))
+        while Tokenizer.get_type(current_char) == next_token.type:
+            next_token.value += self.origin[self.position]
+            self.position += 1
+
+            if self.position == len(self.origin):
+                self.actual = next_token
+                return
+
+            current_char = self.origin[self.position]
+
+        self.actual = next_token
+
+
+
+
+class Parser:
+    @staticmethod
+    def parseExpression(tokenizer):
+        if tokenizer.actual.type == 'int':
+            resultado = int(tokenizer.actual.value)
+            tokenizer.selectNext()
+
+            while tokenizer.actual.value == '+' or tokenizer.actual.value == '-':
+                if tokenizer.actual.value == '+':
+                    tokenizer.selectNext()
+                    if tokenizer.actual.type == 'int':
+                        resultado += int(tokenizer.actual.value)
+                    else:
+                        raise TypeError
+                if tokenizer.actual.value == '-':
+                    tokenizer.selectNext()
+                    if tokenizer.actual.type == 'int':
+                        resultado -= int(tokenizer.actual.value)
+                    else:
+                        raise TypeError
+
+                tokenizer.selectNext()
+            if(tokenizer.actual.type != 'EOF'):
                 raise TypeError
-    i += 1
+            return resultado
+        else:
+            raise TypeError
 
-source = source.replace(" ", "")
-if(source[-1] == '+'  or source[-1] == '-'): raise TypeError
-source = source.replace("+", " + ")
-source = source.replace("-", " - ")
-source = source.split(" ")
+    @staticmethod
+    def run(code):
+        tokenizer = Tokenizer(code)
+        return Parser.parseExpression(tokenizer)
 
-soma = int(source[0])
-for i in range(1, len(source), 2):
-    if source[i] == "+":
-        if source[i + 1] != "":
-            soma += int(source[i + 1])
-    elif source[i] == "-":
-        if source[i + 1] != "":
-            soma -= int(source[i + 1])
-    else:
-        raise TypeError
-print(soma)
+def main():
+    source = sys.argv[1]
+    # source = '   111 1   + 23'
+    print(Parser.run(source))
+
+
+if __name__== "__main__":
+  main()
